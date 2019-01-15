d1edfbf7b4f3c17ab6cd9e79e78e5e4e42239a37
jwhite02 <johnwhite4837@gmail.com>
2019-01-15 08:06:07 -0500

    Updates

---
+++ precache-manifest.027c092fecab9d8bd8c99934015c1b38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fd1ac69d9f308b6a87eb4bf0b0d6cba",
+    "url": "/reactmoviedb/static/media/moss-436037-unsplash.1fd1ac69.jpg"
+  },
+  {
+    "revision": "f42b22ae8661ec9bb4fd",
+    "url": "/reactmoviedb/static/js/runtime~main.f42b22ae.js"
+  },
+  {
+    "revision": "ccde969a8ceb1b246e1e",
+    "url": "/reactmoviedb/static/js/main.ccde969a.chunk.js"
+  },
+  {
+    "revision": "9bd38279e27cd5f583f0",
+    "url": "/reactmoviedb/static/js/1.9bd38279.chunk.js"
+  },
+  {
+    "revision": "ccde969a8ceb1b246e1e",
+    "url": "/reactmoviedb/static/css/main.06dba7bf.chunk.css"
+  },
+  {
+    "revision": "9bd38279e27cd5f583f0",
+    "url": "/reactmoviedb/static/css/1.793c3877.chunk.css"
+  },
+  {
+    "revision": "2d7906d932694d9b8a495ec487af2f24",
+    "url": "/reactmoviedb/index.html"
+  }
+];
